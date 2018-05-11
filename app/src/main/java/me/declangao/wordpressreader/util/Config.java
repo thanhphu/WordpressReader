1afc9baeb7e35fec3614e407e10571bb6829b636
Thanh Phu <thanh.phu@daliworks.net>
2018-05-11 14:04:25 +0900

    Update build to Oreo

--- app/src/main/java/me/declangao/wordpressreader/util/Config.java
+++ app/src/main/java/me/declangao/wordpressreader/util/Config.java
@@ -5,8 +5,8 @@ package me.declangao.wordpressreader.util;
  */
 public class Config {
     // Fill in your own WordPress URL, don't forget the "/" at the end
-    public static final String BASE_URL = "http://URL_TO_YOUR_WORDPRESS_SITE/";
+    public static final String BASE_URL = "https://www.luatsuhanquoc.com/";
 
     public static String CATEGORY_URL = BASE_URL + "?json=get_category_index";
-    public static final String DEFAULT_THUMBNAIL_URL = "http://i.imgur.com/D2aUC3s.png";
+    static final String DEFAULT_THUMBNAIL_URL = "http://i.imgur.com/D2aUC3s.png";
 }
